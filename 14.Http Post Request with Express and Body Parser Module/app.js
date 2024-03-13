d6852c1268c1642e78f28ee673b4b109d01d9642
Jaeseok <wotjr5124@gmail.com>
2024-03-13 11:50:30 +0900

    validation with Joi

--- 14.Http Post Request with Express and Body Parser Module/app.js
+++ 14.Http Post Request with Express and Body Parser Module/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser')
+const Joi = require('joi');
 const app = express();
 
 app.use('/public', express.static(path.join(__dirname, 'static')));
@@ -13,8 +14,18 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     console.log(req.body);
-    //database work here
-    res.json({success: true});
+    const schema = Joi.object().keys({
+        email: Joi.string().trim().email().required(),
+        pwd: Joi.string().min(5).max(10).required()
+    });
+
+    const result = schema.validate(req.body)
+    if (result.error) {
+        console.log(result.error)
+        res.send('an error has occurred');
+    }
+    console.log(result);
+    res.send('successfully posted data')
 }); 
 
 app.listen(3000)
